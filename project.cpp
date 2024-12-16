5731b3c1e4fe5ddb4f370dce0bcb3fd9abe203ab
MTHcd <mathm2551@gmail.com>
2024-12-16 23:43:46 +0100

    Create project.cpp

---
+++ project.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+int main()
+{
+  int var1 = 20;
+  int var2 = 3;
+
+  if (var1 >= var2) {
+          cout << "var1 is greater or equal than var2" << endl;
+  } else {
+          cout << "var1 is not greater or equal than var2" << endl;
+  }
+
+  return 1;
+
+}
