7cea655d18da0f32195b5a4483fb8383fb12840f
MTHcd <mathm2551@gmail.com>
2024-12-19 00:46:28 +0100

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -15,26 +15,34 @@ int main() {
 
 #include <stdlib.h>
 #include <iostream>
-using namespace std;
+#include <time.h>
 
+using namespace std;
 int main()
 {
-  int number=rand()
-  int guess =-1; 
-  int trycount = 0;
-  while(guess != number && trycount < 8)
-    {
-      cout << "Please enter a guess: ";
-      cin >> guess;
-      if (guess_number < number) {
-        cout << "Too low" << endl;
-      } else if (guess_number > number) {
-        cout << "Too high" << endl;
-      } else if (guess_number == number) {
-        cout << "You guessed the number";
-        return 1;
-      }
-    }
-  cout << "Sorry, the number was: " << number;
-  return 0;  
+srand(time(NULL));    
+int number=rand()%100; 
+int guess=-1;
+int trycount=0;
+while(guess!=number && trycount<8)
+
+{
+cout<<"Please enter a guess: ";
+cin>>guess;
+
+if(guess<number)
+cout<<"Too low"<<endl;
+
+if(guess>number)
+ cout<<"Too high"<<endl;
+
+trycount++;
 }
+if(guess==number)
+cout<<"You guessed the number";
+else
+cout<<"Sorry, the number was: "<<number;
+return 0;
+}
+
+
