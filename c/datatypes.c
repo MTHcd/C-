f70a61de4946a652c164b696f2509b0f7396f2d8
MTHcd <mathm2551@gmail.com>
2025-01-01 02:46:14 +0100

    Create datatypes.c

---
+++ c/datatypes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  
+  // checking: int, char, float, double, void
+  int var = 3;
+  
+  float var1 = 2.4;
+  
+  double var2 = 2.443;
+  // checking double datatype:
+
+  //Remark on difference between double/float/int
+  // double vs float
+  // +, - : occupied memory larger for double than float
+  // -, +: precision is better for double than for float
+  
+  // int => signed vs unsigned
+  unsigned int unsignedVar = 10; // unsigned
+  int signedVar = -10; // signed
+
+  //Checking string vs char datatype in C
+  // char
+  
+  return 0;
+}
